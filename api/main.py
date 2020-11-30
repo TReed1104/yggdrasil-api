aa33a87d0e5541612451a6d36cabb593378317c1
Tom Reed <t.reed1104@gmail.com>
2020-11-30 13:40:21 +0000

    Amended or removed template source files

--- api/main.py
+++ api/main.py
@@ -4,7 +4,6 @@ from flask_restful import Api
 from flask_cors import CORS
 from models import example_model
 from shared import db
-from resources import template_resource
 
 ## Create our Flask app and connect it to our database
 app = Flask(__name__)
@@ -14,9 +13,6 @@ CORS(app)
 db.init_app(app)
 with app.app_context():
     db.create_all()
-
-## Register the resources and the endpoints to access them
-api.add_resource(template_resource.TemplateResource, '/example')
 
 if __name__ == '__main__':
     ## Initialise the application, 0.0.0.0 means to use our machine ip and enable debugging if needed
