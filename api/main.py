ca8ff36f690229c705a9564fd23e2a29e8c4a195
Tom Reed <t.reed1104@gmail.com>
2020-12-01 11:42:19 +0000

    Migrated app core

--- api/main.py
+++ api/main.py
@@ -2,8 +2,10 @@
 from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
-from models import example_model
+from models import carousel, content
 from shared import db
+from resources import list_carousels, list_content
+from resources import handler_carousel, handler_content
 
 ## Create our Flask app and connect it to our database
 app = Flask(__name__)
@@ -13,6 +15,12 @@ CORS(app)
 db.init_app(app)
 with app.app_context():
     db.create_all()
+
+## Register the resources and the endpoints to access them
+api.add_resource(list_carousels.CarouselList, '/carousels')
+api.add_resource(list_content.ContentList, '/carouselcontent')
+api.add_resource(handler_carousel.CarouselHandler, '/carousel_handler')
+api.add_resource(handler_content.ContentHandler, '/content_handler')
 
 if __name__ == '__main__':
     ## Initialise the application, 0.0.0.0 means to use our machine ip and enable debugging if needed
